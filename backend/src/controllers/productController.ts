b15baf17f37fc8c71e348ddc0493d4eecdbe154d
wesleydematos <wesleydematos3@gmail.com>
2023-09-05 19:20:38 -0300

    chore: added multer

--- backend/src/controllers/productController.ts
+++ backend/src/controllers/productController.ts
@@ -2,8 +2,20 @@ import { Request, Response, Router } from "express";
 import Product from "../entities/Product";
 import { getProducts } from "../services/productSevices";
 import IProducts from "../interfaces/IProducts";
+import multer from "multer";
+
+const multerConfig = multer();
 
 export const productRouter = Router();
+
+productRouter.post(
+  "/",
+  multerConfig.single("file"),
+  async (req: Request, res: Response) => {
+    console.log(req.file.buffer.toString("utf-8"));
+    return res.send();
+  }
+);
 
 productRouter.get(
   "/",
