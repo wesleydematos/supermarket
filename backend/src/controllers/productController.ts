18b3ff3fe244c9eded5a864513fdc32465e92f8c
wesleydematos <wesleydematos3@gmail.com>
2023-09-06 19:13:42 -0300

    feat: created multer route

--- backend/src/controllers/productController.ts
+++ backend/src/controllers/productController.ts
@@ -3,17 +3,54 @@ import Product from "../entities/Product";
 import { getProducts } from "../services/productSevices";
 import IProducts from "../interfaces/IProducts";
 import multer from "multer";
+import { Readable } from "stream";
+import readline from "readline";
 
 const multerConfig = multer();
 
 export const productRouter = Router();
 
+interface IProduct {
+  code: number;
+  sales_price: number;
+}
+
 productRouter.post(
   "/",
   multerConfig.single("file"),
   async (req: Request, res: Response) => {
-    console.log(req.file.buffer.toString("utf-8"));
-    return res.send();
+    const { file } = req;
+    const { buffer } = file;
+
+    const readableFile = new Readable();
+    readableFile.push(buffer);
+    readableFile.push(null);
+
+    const productsLine = readline.createInterface({
+      input: readableFile,
+    });
+
+    const products: IProduct[] = [];
+
+    let firstInteraction = true;
+
+    for await (let line of productsLine) {
+      const productLineSplit = line.split(",");
+
+      if (firstInteraction) {
+        // console.log(productLineSplit);
+        // [ 'product_code', 'new_price' ]
+        firstInteraction = false;
+        continue;
+      }
+
+      products.push({
+        code: Number(productLineSplit[0]),
+        sales_price: Number(productLineSplit[1]),
+      });
+    }
+
+    return res.json(products);
   }
 );
 
